e5f25d05e2e0299552a6748eba55591cd20dc846
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-01-22 01:05:13 -0500

    Update winstart.sh

--- winstart.sh
+++ winstart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-i386 -netdev user,id=mynet0 -device ne2k_isa,netdev=mynet0 -hda win95.qcow2 -soundhw sb16 -m 64 -cpu pentium -vga cirrus -boot c -soundhw pcspk
+qemu-system-i386 -netdev user,id=mynet0 -device ne2k_isa,netdev=mynet0 -hda win95.qcow2 -device sb16 -m 64 -cpu pentium -vga cirrus -boot c -soundhw pcspk
