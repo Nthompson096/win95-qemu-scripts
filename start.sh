e7bd02a2430f775590123e3ddb7e7642a50d0dbe
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-01-23 16:24:05 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,16 +8,11 @@ chmod +x win95.sh |
 
 chmod +x w95cpu.sh |
 
-chmod +x winstartnokvm.sh
-
-wget 'https://winworldpc.com/download/db57f85a-f039-11e7-a562-fa163e9022f0/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2' -O ./bootdisc.7z |
+chmod +x winstartnokvm.sh |
 
 wget 'https://dl-alt1.winworldpc.com/Microsoft%20Windows%2095B%20(4.00.1111)%20(OSR2)%20[English]%20(CD).7z' -O ./instdisc.7z &&
 
-
 7z e ./instdisc.7z &&
-
-7z e ./bootdisc.7z &&
 
 rm ./instdisc.7z |
 
@@ -27,9 +22,7 @@ rm -r 'Microsoft Windows 95B (4.00.1111) (OSR2) [English] (CD)' |
 
 rm winworldpc.com.txt |
 
-rm media-disk01.jpg |
-
-rm ./bootdisc.7z &&
+rm media-disk01.jpg &&
 
 	./windisc.sh &&
 	./win95.sh &&
