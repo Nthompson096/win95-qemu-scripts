3ad626ae6ac1b03cab24ed4fbcede5e9d39a9a4c
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-01-23 20:28:57 -0500

    Update winstartkvm.sh

--- winstartkvm.sh
+++ winstartkvm.sh
@@ -2,7 +2,7 @@
 
 	qemu-system-i386 \
 	-device ne2k_isa,netdev=win95net0 \
-	-netdev user,id=win95net0,net=192.168.0.0/16,dhcpstart=192.168.0.1,restrict=off \
+	-netdev user,id=win95net0,restrict=off \
 	-drive file=win95.qcow2,format=qcow2 \
 	-m 64 \
 	-cpu pentium \
