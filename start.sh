d1772c41be145b442f14d6f1cc877fa61252d532
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-01-24 00:25:11 -0500

    added IE4.0

--- start.sh
+++ start.sh
@@ -16,6 +16,8 @@ chmod +x ie95.sh |
 
 wget 'https://dl-alt1.winworldpc.com/Microsoft%20Windows%2095B%20(4.00.1111)%20(OSR2)%20[English]%20(CD).7z' -O ./instdisc.7z &&
 
+wget 'https://archive.org/download/ie4-win95-winnt/Internet%20Explorer%204.0%20for%20Windows%2095%20and%20NT%204.0.iso' -O ./iew95.iso &&
+
 7z e ./instdisc.7z &&
 
 rm ./instdisc.7z |
