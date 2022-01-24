9c4cc90cf0a71cef5e02a911bd8230c489d8d7de
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-01-23 19:11:10 -0500

    virtual network

--- win95.sh
+++ win95.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-qemu-system-i386 -netdev user,id=mynet0 -device ne2k_isa,netdev=mynet0 -hda win95.qcow2 -device sb16 -m 64 -cpu pentium -vga cirrus -fda disk01.img -cdrom windows95b.iso -boot a -soundhw pcspk
+qemu-system-i386 \
+	-netdev user,id=win95net0 \
+	-device ne2k_isa,netdev=win95net0 \
+       	-netdev user,id=win95net0,net=192.168.0.0/16,dhcpstart=192.168.0.1,restrict=off	\
+	-drive file=win95.qcow2,format=qcow2 \
+	-soundhw sb16 \
+	-m 64 \
+	-cpu pentium \
+	-vga cirrus \
+	-fda disk01.img \
+	-cdrom windows95b.iso \
+	-boot a \
+	-soundhw pcspk
