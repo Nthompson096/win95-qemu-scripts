6a4069c56a01fd68a2ccc078328a2b269fb00909
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-05-21 06:44:05 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,25 +9,21 @@ else
 #Check if there's a dir, if there is; I will create a dir and copy files, if not skip.	
 
 [[ ! -d /usr/share/win95 ]] 
-mkdir /usr/share/win95 2> /dev/null && chmod +x ./*.sh && cp ./* /usr/share/win95 
-
-
-
-# if [[ ! -d /usr/share/win95 ]]; then
-# 		mkdir /usr/share/win95 && chmod +x ./*.sh && cp ./* /usr/share/win95 ||
-# 	 [[ -d /usr/share/win95 ]]; then
-# echo ""
-
+mkdir /usr/share/win95 2> /dev/null && 
+chmod +x ./*.sh && 
+cp ./* /usr/share/win95 
 
 #Yo dawg where's the install disk? if it's here I'll see if the disc is downloaded anyway...
  
  [[ ! -f /usr/share/win95/ie95.iso ]]
 #touch /usr/share/win95/ie95.iso
-	echo "grabbing ie 95" && wget -c 'https://archive.org/download/ie4-win95-winnt/Internet%20Explorer%204.0%20for%20Windows%2095%20and%20NT%204.0.iso' -O /usr/share/win95/ie95.iso
+	echo "grabbing ie 95" && 
+	wget -c 'https://archive.org/download/ie4-win95-winnt/Internet%20Explorer%204.0%20for%20Windows%2095%20and%20NT%204.0.iso' -O /usr/share/win95/ie95.iso
 	
 	if  [[ -f /usr/share/win95/ie95.iso ]]; then 
 			#/usr/share/win95/ie95.iso
-			echo "grabbing ie 95" && wget -c 'https://archive.org/download/ie4-win95-winnt/Internet%20Explorer%204.0%20for%20Windows%2095%20and%20NT%204.0.iso' -O /usr/share/win95/ie95.iso 2> /dev/null
+			echo "grabbing ie 95" && 
+			wget -c 'https://archive.org/download/ie4-win95-winnt/Internet%20Explorer%204.0%20for%20Windows%2095%20and%20NT%204.0.iso' -O /usr/share/win95/ie95.iso 2> /dev/null
 
 	
 
@@ -49,15 +45,30 @@ done
 #Will format even if not missing...
 
 	if [[ ! -f /var/lib/libvirt/images/win95.qcow2 ]]; then
-		qemu-img create -f qcow2 /var/lib/libvirt/images/win95.qcow2 2G && chmod a+rwX /var/lib/libvirt/images/win95.qcow2 && echo -e "\n\n\n\n" && cat /usr/share/win95/key.txt && echo -e "\n\n\n\n" && ./win95.sh && ./win95.sh && /usr/share/win95/winstartnokvm.sh
+		qemu-img create -f qcow2 /var/lib/libvirt/images/win95.qcow2 2G && 
+		chmod a+rwX /var/lib/libvirt/images/win95.qcow2 && 
+		echo -e "\n\n\n\n" && 
+		cat /usr/share/win95/key.txt && 
+		echo -e "\n\n\n\n" &&
+		 ./win95.sh && 
+		 ./win95.sh && 
+		 /usr/share/win95/winstartnokvm.sh
 
 	else [[ -f /var/lib/libvirt/images/win95.qcow2 ]]; #then
-		# echo "Win95 HDD exists, restart install?" &&  qemu-img create -f qcow2 /var/lib/libvirt/images/win95.qcow2 2G && chmod a+rwX /var/lib/libvirt/images/win95.qcow2 && echo -e "\n\n\n\n" && cat /usr/share/win95/key.txt && echo -e "\n\n\n\n" && ./win95.sh  && ./win95.sh && /usr/share/win95/winstartnokvm.sh
 		echo "Win95 HDD exists, format virtual drive?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) qemu-img create -f qcow2 /var/lib/libvirt/images/win95.qcow2 2G && chmod a+rwX /var/lib/libvirt/images/win95.qcow2 && echo -e "\n\n\n\n" && cat /usr/share/win95/key.txt && echo -e "\n\n\n\n" && ./win95.sh  && ./win95.sh && /usr/share/win95/winstartnokvm.sh; break;;
-        No ) cat /usr/share/win95/key.txt && echo -e "\n\n\n\n" && ./win95.sh  && ./win95.sh && /usr/share/win95/winstartnokvm.sh; break;;
+        Yes ) qemu-img create -f qcow2 /var/lib/libvirt/images/win95.qcow2 2G && 
+		chmod a+rwX /var/lib/libvirt/images/win95.qcow2 && echo -e "\n\n\n\n" && 
+		cat /usr/share/win95/key.txt && echo -e "\n\n\n\n" && 
+		./win95.sh  && 
+		./win95.sh && 
+		/usr/share/win95/winstartnokvm.sh; break;;
+        No ) cat /usr/share/win95/key.txt && 
+		echo -e "\n\n\n\n" && 
+		./win95.sh  && 
+		./win95.sh && 
+		/usr/share/win95/winstartnokvm.sh; break;;
     esac
 done
 
