357f0950d97431c92dd51c732a90165b9de2f833
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-05-22 08:44:43 -0400

    Update uninst95.sh

--- uninst95.sh
+++ uninst95.sh
@@ -11,7 +11,7 @@ fi
 echo "Are you sure you want to completely remove windows 95?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) rm /usr/bin/win95kvm && rm /usr/bin/win95nokvm && rm /var/lib/libvirt/images/win95.qcow2 && echo "win95 uninstalled." && rm /usr/bin/uninst95.sh && rm -r /usr/share/win95; break;;
+        Yes ) rm /usr/bin/win95kvm && rm /usr/bin/win95nokvm && rm /var/lib/libvirt/images/win95.qcow2 && echo "win95 uninstalled." && rm /usr/bin/uninst95 && rm -r /usr/share/win95/; break;;
         No ) exit;;
     esac
 done
