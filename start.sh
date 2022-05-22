5dbdb664eda2afa2a839ae8f85f495bffdc1742e
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-05-22 19:21:30 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -76,6 +76,7 @@ echo "Are you trying to continue an install"
 
 select yn in "Yes" "No"; do
     case $yn in
+ 		
  		Yes ) cat /usr/share/win95/key.txt &
 		/usr/share/win95/win95.sh 2> /dev/null && 
 		/usr/share/win95/winstartnokvm.sh 2> /dev/null; break;;
