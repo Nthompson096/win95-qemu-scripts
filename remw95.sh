dd7218fa05aef3fccc338ec80717d6c86bb3faf6
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-05-24 02:19:47 -0400

    Update remw95.sh

--- remw95.sh
+++ remw95.sh
@@ -5,8 +5,8 @@ if [[ -f /usr/bin/win95kvm ]]; then
 				 rm /usr/bin/win95nokvm 
 				 [[ -f /usr/bin/uninst95 ]] 
 				    rm /usr/bin/uninst95
+				[[ -f /var/lib/libvirt/images/win95.qcow2 ]]
+				     rm /var/lib/libvirt/images/win95.qcow2
 				    [[ -d /usr/share/win95/ ]] 
-				     rm -r /usr/share/win95/
-				     [[ -f /var/lib/libvirt/images/win95.qcow2 ]]
-				     rm /var/lib/libvirt/images/win95.qcow2;				    
+				     rm -r /usr/share/win95/;				    
 fi
