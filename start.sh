c1d8a8001be71096a939771dbf21f646f63980ae
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-05-24 02:14:36 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,47 +6,46 @@
 	  then echo "Please run sudo."
 		    exit
 else
-#Check if there's a dir, if there is; I will create a dir and copy files, if not skip.
-#Create shortcuts when launched.
+#Check if there's a dir, if there is; I will create a dir and copy files, if not skip.	
 
 [[ ! -d /usr/share/win95 ]]
 mkdir /usr/share/win95 2> /dev/null && 
 chmod +x ./*.sh &&
 cp ./* -u /usr/share/win95/ &
 
-	if [[ ! -f /usr/bin/win95nokvm ]]
+[[ ! -f /usr/bin/win95nokvm ]]
 		echo "Creating shortcuts in /usr/bin for no win95kvm" && ln -sf /usr/share/win95/winstartnokvm.sh /usr/bin/win95nokvm 
-
-	  [[ -f /usr/bin/win95nokvm ]]; then
+[[ -f /usr/bin/win95nokvm ]]
 		 echo "Created no KVM shotcuts"
 
 
-	if [[ ! -f /usr/bin/win95kvm ]]
+[[ ! -f /usr/bin/win95kvm ]]
 		echo "Creating KVM shortcuts for win 95" && ln -sf /usr/share/win95/winstartkvm.sh /usr/bin/win95kvm 
 
-	 [[ -f /usr/bin/win95kvm ]]; then
+[[ -f /usr/bin/win95kvm ]]
 		echo "Created KVM shortcuts for windows 95"
 
-	if [[ ! -f /usr/bin/uninst95 ]]
+[[ ! -f /usr/bin/uninst95 ]]
 		echo "Creating the uninstall terminal shortcut." && ln -sf /usr/share/win95/uninst95.sh /usr/bin/uninst95 
 
-	   [[ -f /usr/bin/uninst95 ]]; then
+[[ -f /usr/bin/uninst95 ]]
 		echo "Created the uninstall terminal shortcut."
 
-		if [[ ! -f /usr/bin/inst95 ]]
+[[ ! -f /usr/bin/inst95 ]]
 		echo "Creating the install terminal shortcut." && ln -sf /usr/share/win95/start.sh /usr/bin/inst95
 
-	   [[ -f /usr/bin/inst95 ]]; then
+[[ -f /usr/bin/inst95 ]]
 		echo "Created the install terminal shortcut."
+
 
 #Yo dawg where's the install disk? if it's here I'll see if the disc is downloaded anyway...
  
- [[ ! -f /usr/share/win95/ie95.iso ]]
+ if [[ ! -f /usr/share/win95/ie95.iso ]]; then
 #touch /usr/share/win95/ie95.iso
 	echo "grabbing ie 95" && 
 	wget -c 'https://archive.org/download/ie4-win95-winnt/Internet%20Explorer%204.0%20for%20Windows%2095%20and%20NT%204.0.iso' -O /usr/share/win95/ie95.iso
 
-	if  [[ -f /usr/share/win95/ie95.iso ]]; then 
+ [[ -f /usr/share/win95/ie95.iso ]]
 			#/usr/share/win95/ie95.iso
 			echo "Redownload IE-95?"
 
@@ -58,7 +57,7 @@ select yn in "Yes" "No"; do
 done
 	
 
-#else if doesn't work, try elif, no doesn't work... hmmm, else works..
+#else if doesn't work, try elif, no doesn't work... hmmm, else works.. NOT
 
 	 [[ ! -f /usr/share/win95/instdisc.iso ]]; #then
 		wget -c 'https://archive.org/download/microsoft-windows95-osr2/windows95osr2.iso' -O /usr/share/win95/instdisc.iso
@@ -110,15 +109,6 @@ select yn in "Yes" "No"; do
     esac
 done
 
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-fi
-
 echo "want to install additonal drivers for windows 95?"
 select yn in "Yes" "No"; do
     case $yn in
@@ -126,3 +116,8 @@ select yn in "Yes" "No"; do
         No ) exit;;
     esac
 done
+
+fi
+fi
+fi
+fi
