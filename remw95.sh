685a6632fdb6fe33c32953bbfab46cca7fbf0577
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-06-05 14:50:50 -0400

    Update remw95.sh

--- remw95.sh
+++ remw95.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 	
 	if [ -f /usr/bin/win95kvm ]; then
-	rm /usr/bin/win95kvm fi
-	
-	if [ -f /usr/bin/win95nokvm ]; then
-	rm /usr/bin/win95nokvm fi
-	
-	if [ -f /usr/bin/uninst95 ] ; then
-	rm /usr/bin/uninst95 fi
+	rm /usr/bin/win95kvm 
+	fi
 
-	if [ -f /usr/bin/inst95 ] ; then
-	rm /usr/bin/inst95 fi
+	if [ -f /usr/bin/win95nokvm ]; then
+	rm /usr/bin/win95nokvm 
+	fi
+	
+	if [ -f /usr/bin/uninst95 ]; then
+	rm /usr/bin/uninst95 
+	fi
+
+	if [ -f /usr/bin/inst95 ]; then
+	rm /usr/bin/inst95 
+	fi
 	
 	if [ -f /usr/bin/win95cpu ]; then
-	rm /usr/bin/win95cpu fi
+	rm /usr/bin/win95cpu 
+	fi
 	
 	if [ -f /var/lib/libvirt/images/win95.qcow2 ]; then
-	rm /var/lib/libvirt/images/win95.qcow2 fi
+	rm /var/lib/libvirt/images/win95.qcow2 
+	fi
     
     if [ -d /usr/share/win95/ ]; then 
-	rm -r /usr/share/win95/ fi
+	rm -r /usr/share/win95/ 
+	fi
